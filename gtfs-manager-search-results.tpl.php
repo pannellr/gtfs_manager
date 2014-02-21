c1db0dc2d4309c1d2fcbc39805080f380357229f
CodeFusion1 <codefusion1@gmail.com>
2014-02-21 01:48:16 -0400

    Soft Launch Version
    Final commit before soft launch

--- gtfs-manager-search-results.tpl.php
+++ gtfs-manager-search-results.tpl.php
@@ -3,10 +3,7 @@
  * @file search results page template
  */
 ?>
-<section class="main">
-	<div class="container">
-		<aside class="left-col">
-			<div class="trip-holder">
+	<div class="trip-holder departing">
 				<!--<p><small><em>Fare prices are subject to chage.</em></small></p>
    <a href="#" class="change-date prev">Previous day</a> |
   <a href="#" class="change-date next">Next day</a>-->
@@ -22,14 +19,23 @@
 					</ul>
 					<ul class="trip-date">
 						<li><i class="icon-calendar-empty"></i> <?php print $date; ?></li>
-
+						<?php if(strtotime('-1 day', $timestamp) >= strtotime('today midnight')){?>
+						<li><a class="btn" href="<?php echo "/gtfs_search/$fromcity/$fromprov/$tocity/$toprov/" . strtotime('-1 day', $timestamp) . "/$return_timestamp"; ?>">Prev day</a></li>
+						<?php } else {?>
+						<li><span class="btn disabled">Prev Day</span></li>
+						<?php } ?>
+						<?php if(isset($return_timestamp) && strtotime('+1 day', $timestamp) > $return_timestamp){?>
+						<li><span class="btn disabled">Next Day</span></li>
+						<?php } else { ?>
+						<li><a class="btn" href="<?php echo "/gtfs_search/$fromcity/$fromprov/$tocity/$toprov/" . strtotime('+1 day', $timestamp) . "/$return_timestamp"; ?>">Next day</a></li>
+						<?php } ?>
 					</ul>
 				</div>
 				<div class="table-holder">
+					<img src="/sites/all/themes/atlantica/img/gesture.png" alt="Swipe Left" class="table-gesture">
 					<table class="trip-table departing">
 						<thead>
 							<tr>
-								<th class="center"><i class="icon-ok"></i></th>
 								<th>Depart</th>
 								<th>Arrive</th>
 								<th>Duration</th>
@@ -47,7 +53,6 @@
 							<?php $fromaddr = str_replace($from . '- ', '', str_replace($from . ' - ', '', $result->departure_description));
 	      	$toaddr = str_replace($to . '- ', '', str_replace($to . ' - ', '', $result->destination_description)); ?>
 							<tr>
-								<td class="center"><input type="radio" name="departureSel" value="depart-<?php print $i?>"></td>
 								<td><?php print $result->departure_departure_time; ?></td>
 								<td><?php print $result->destination_arrival_time; ?></td>
 								<td><?php print $result->duration . $result->days; ?></td>
@@ -55,7 +60,7 @@
 								<td class="center"><?php print $result->transfer;?></td>
 								<td><?php print $result->features;?></td>
 								<td><?php print $result->transportation_type; ?></td>
-								<td><?php print $result->departure_agency_name; ?></td>
+								<td><?php print stripslashes($result->provider); ?></td>
 								<td class="center"><a
 									data-expander="#trip-<?php print $i; ?>-details" href="#"><i
 										class="icon-chevron-down"></i> </a>
@@ -89,10 +94,10 @@
 												
 												</li>
 												<li class="cost"><h6>
-														<?php print $result->price; ?>
+														<?php (isset($result->num_transfers) ? print $result->first_leg_price : print $result->price); ?>
 													</h6></li>
 												<li class="transporter"><a target="_blank"
-													href="<?php print $result->departure_agency_url; ?>"><?php print $result->departure_agency_name; ?>
+													href="<?php print $result->departure_agency_url; ?>"><?php print stripslashes($result->departure_agency_name); ?>
 														<i class="icon-external-link"></i> </a>
 													<p>
 														<?php print $result->agency_phone; ?>
@@ -100,6 +105,31 @@
 												</li>
 											</ul>
 										</li>
+										<?php if($result->num_transfers){?>
+											<li class="transfer">
+												<h4><i class="icon-random"></i> Transfer</h4>
+												<ul>
+													<li class="time"><h2><?php print $result->departure_transfer_departure_time; ?></h2></li>
+													<li class="location">
+														<h6>Arrive at <?php print $result->from_stop_name; ?></h6>
+													</li>
+													<li class="transporter">
+														<p><small><i class="icon-time"></i> Layover: <?php print $result->layover_duration; ?></small></p>
+													</li>
+												</ul>
+											</li>
+											<li>
+												<ul>
+													<li class="time"><h2><?php print $result->destination_transfer_arrival_time; ?></h2></li>
+													<li class="location"><h6>Depart from <?php print $result->to_stop_name; ?></h6></li>
+													<li class="cost"><h6><?php print $result->second_leg_price; ?></h6></li>
+													<li class="transporter">
+														<a href="<?php print $result->destination_agency_url; ?>"><?php print stripslashes($result->destination_agency_name); ?> <i class="icon-external-link"></i></a>
+														<p><?php print $result->destination_agency_phone; ?>
+													</li>
+												</ul>
+											</li>
+										<?php } ?>
 										<li>
 											<ul>
 												<li class="time"><h2>
@@ -133,132 +163,136 @@
 						</tbody>
 					</table>
 				</div>
-
-				<?php if (!empty($return_results)) { ?>
-				<h1>Return Trip</h1>
-				<h2>
-					<?php print $to; ?>
-					to
-					<?php print $from; ?>
-					-
-					<?php print $return_date; ?>
-				</h2>
-				<a href="#" class="change-date prev return">Previous day</a> | <a
-					href="#" class="change-date next return">Next day</a>
-				<table>
-					<thead>
-						<tr>
-							<th class="center"><i class="icon-ok"></i></th>
-							<th>Depart</th>
-							<th>Arrive</th>
-							<th>Duration</th>
-							<th>Cost*</th>
-							<th class="center">Transfers</th>
-							<th>Features</th>
-							<th>Type</th>
-							<th>Transporter</th>
-							<th class="center">Info</th>
-						</tr>
-					</thead>
-					<tbody>
-						<?php $i = 1; ?>
-						<?php foreach ($return_results as $result) { ?>
-						<tr>
-							<td><input type="radio" name="returnSel" value="return-<?php print $i?>"></td>
-							<td><?php print $result->departure_departure_time; ?></td>
-							<td><?php print $result->destination_arrival_time; ?></td>
-							<td><?php print $result->duration; ?></td>
-							<td><?php print $result->price; ?></td>
-							<td><?php print $result->transfer;?></td>
-							<td><?php print $result->features;?></td>
-							<td><?php print $result->transportation_type; ?></td>
-							<td><?php print $result->provider; ?></td>
-							<td class="center"><a
-								data-expander="#trip-<?php print $i; ?>-details" href="#"><i
-									class="icon-chevron-down"></i></span>
-							
-							</td>
-						</tr>
-						<tr id="trip-<?php print $i; ?>-details" class="expandable">
-							<td colspan="9"><?php print $result->more_info; ?></td>
-						</tr>
-						<?php $i++; ?>
-						<?php } ?>
-					</tbody>
-				</table>
-
-				<?php } ?>
 			</div>
-		</aside>
-		<aside class="right-col">
-			<section class="block legend">
-				<h3>Feature Legend</h3>
-				<ul class="legend-list">
-					<li><span><img alt="Bike Friendly"
-							src="/sites/all/themes/atlantica/img/features/feature-bike-w.png">
-					</span> Ability to carry your bike</li>
-					<li><span><img alt="Accessible"
-							src="/sites/all/themes/atlantica/img/features/feature-accessible-w.png"></i>
-					</span> Accessible</li>
-					<li><span><img alt="WiFi"
-							src="/sites/all/themes/atlantica/img/features/feature-wifi-w.png">
-					</span> Free onboard Wi-Fi</li>
-				</ul>
-			</section>
-			<section class="block cost-outline">
-				<h3>Total Cost Outline</h3>
-				<ul class="total-cost departure">
-					<li>
-						<h2>
-							Depart <strong>Oct. 21, 2013</strong>
-						</h2>
-						<h1>Halifax (NS)</h1>
-					</li>
-					<li>
-						<h4>$24.10</h4>
-						<h3>9:00am</h3>
-					</li>
-				</ul>
-				<ul class="total-cost return">
-					<li>
-						<h2>
-							Return <strong>Oct. 23, 2013</strong>
-						</h2>
-						<h1>Truro (NS)</h1>
-					</li>
-					<li>
-						<h4>$24.10</h4>
-						<h3>10:30am</h3>
-					</li>
-				</ul>
-				<ul class="total-cost final-cost">
-					<li>
-						<p>
-							<small>HST included.</small>
-						</p>
-					</li>
-					<li>
-						<h1>$48.20*</h1>
-					</li>
-				</ul>
-				<p class="disclaimer">
-					<small>* Prices are subject to change without notice.</small>
-				</p>
-				<div id="book-instructions">
-					<h6>Booking Instructions</h6>
-					<ol>
-						<li>Call <a href="#">Maritime Bus</a> at 1-800-465-4655 at least
-							48 hours prior to your trip.
-						</li>
-						<li>Reserve your return trip with <a href="#">VIA Rail</a> with
-							their online booking system.
-						</li>
-						<li>Enjoy the ride!</li>
-					</ol>
+				<?php if (!empty($return_results)) { ?>
+			<div class="trip-holder return">
+				<div class="trip-title">
+					<ul class="trip-text">
+						<li><h5>Your Return Trip</h5></li>
+						<li><h1>
+								<i class="icon-map-marker"></i>
+								<?php print $to; ?>
+								to
+								<?php print $from; ?>
+							</h1></li>
+					</ul>
+					<ul class="trip-date">
+						<li><i class="icon-calendar-empty"></i> <?php print $return_date; ?></li>
+						<?php if(strtotime('-1 day', $return_timestamp) >= $timestamp){?>
+							<li><a class="btn" href="<?php echo "/gtfs_search/$fromcity/$fromprov/$tocity/$toprov/$timestamp/" . strtotime('-1 day', $return_timestamp); ?>">Prev day</a></li>
+						<?php } else { ?>
+							<li><span class="btn disabled">Prev Day</span></li>
+						<?php } ?>
+						<li><a class="btn" href="<?php echo "/gtfs_search/$fromcity/$fromprov/$tocity/$toprov/$timestamp/" . strtotime('+1 day', $return_timestamp); ?>">Next day</a></li>
+					</ul>
 				</div>
-				<a href="#" class="btn book-now" id="instruction-expand"><i
-					class="icon-ok"></i> Book Your Trip</a>
-			</section>
-		</aside>
-	</div>
-</section>
+				<div class="table-holder">
+					<img src="/sites/all/themes/atlantica/img/gesture.png" alt="Swipe Left" class="table-gesture">
+					<div class="trip-table return">
+						<table>
+							<thead>
+								<tr>
+									<th>Depart</th>
+									<th>Arrive</th>
+									<th>Duration</th>
+									<th>Cost*</th>
+									<th class="center">Transfers</th>
+									<th>Features</th>
+									<th>Type</th>
+									<th>Transporter</th>
+									<th class="center">Info</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php $i = 1; ?>
+								<?php foreach ($return_results as $result) { ?>
+								<tr>
+									<td><?php print $result->departure_departure_time; ?></td>
+									<td><?php print $result->destination_arrival_time; ?></td>
+									<td><?php print $result->duration; ?></td>
+									<td><?php print $result->price; ?></td>
+									<td class="center"><?php print $result->transfer;?></td>
+									<td><?php print $result->features;?></td>
+									<td><?php print $result->transportation_type; ?></td>
+									<td><?php print stripslashes($result->provider); ?></td>
+									<td class="center"><a
+										data-expander="#trip-return-<?php print $i; ?>-details" href="#"><i
+											class="icon-chevron-down"></i></a>
+									
+									</td>
+								</tr>
+								<tr id="trip-return-<?php print $i; ?>-details" class="expandable">
+								<td colspan="9">
+									<ul class="trip-breakdown-title return">
+										<li><h1>
+												<span>A</span>
+											</h1>
+										
+										<li>
+											<h1>
+												<?php print $to; ?>
+											</h1>
+										</li>
+									</ul>
+									<ul class="trip-breakdown">
+										<li>
+											<ul>
+												<li class="time"><h2>
+														<?php print $result->departure_arrival_time; ?>
+													</h2></li>
+												<li class="location"><h6>
+														Depart from
+														<?php print $to; ?>
+													</h6>
+													<p>
+														<?php print $toaddr; ?>
+												
+												</li>
+												<li class="cost"><h6>
+														<?php print $result->price; ?>
+													</h6></li>
+												<li class="transporter"><a target="_blank"
+													href="<?php print $result->departure_agency_url; ?>"><?php print stripslashes($result->departure_agency_name); ?>
+														<i class="icon-external-link"></i> </a>
+													<p>
+														<?php print $result->agency_phone; ?>
+												
+												</li>
+											</ul>
+										</li>
+										<li>
+											<ul>
+												<li class="time"><h2>
+														<?php print $result->destination_arrival_time; ?>
+													</h2></li>
+												<li class="location"><h6>
+														Arrive at
+														<?php print $from; ?>
+													</h6>
+													<p>
+														<?php print $fromaddr; ?>
+												
+												</li>
+											</ul>
+										</li>
+									</ul>
+									<ul class="trip-breakdown-title arrival">
+										<li><h1>
+												<span>B</span>
+											</h1></li>
+										<li>
+											<h1>
+												<?php print $from; ?>
+											</h1>
+										</li>
+									</ul>
+								</td>
+							</tr>
+								<?php $i++; ?>
+								<?php } ?>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+			<?php } ?>
